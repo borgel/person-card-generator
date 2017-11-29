0bed070c35c1ed9b8d420470d4ea71a55b6118cf
Kerry Scharfglass <iborgel@gmail.com>
2017-11-28 19:57:10 -0800

    Use nice fonts

--- cardgen.py
+++ cardgen.py
@@ -1,14 +1,23 @@
 #!/usr/local/bin/python
 
 import sys, os, glob, csv
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 INPUT_FILE = ""
 OUTPUT_DIR = "./cards/"
 
 MIN_IMAGE_SIZE = (411, 561)
 SIZE_MULT = 2
+IMAGE_SIZE = tuple([SIZE_MULT * x for x in MIN_IMAGE_SIZE])
 IMAGE_FORMAT = "png"
+
+FONT_NAME = 'Georgia.ttf'
+FONT_SIZE_BIG     = 60
+FONT_SIZE_MEDIUM  = 40
+FONT_SIZE_SMALL   = 20
+
+# TODO get headshot (in drawable we can mix)
+# TODO allow add stroke or boder
 
 def GenerateCard(idx, person):
    '''
@@ -19,13 +28,17 @@ def GenerateCard(idx, person):
    print(person)
 
    # empty image we will work on top of
-   base = Image.new("RGB", MIN_IMAGE_SIZE)
+   base = Image.new("RGB", IMAGE_SIZE)
 
    back = ImageDraw.Draw(base)
-   back.rectangle([(0, 0), MIN_IMAGE_SIZE], fill='White')
+   back.rectangle([(0, 0), IMAGE_SIZE], fill='White')
+
+   # get a font
+   fontBig = ImageFont.truetype(FONT_NAME, FONT_SIZE_BIG)
 
    name = ImageDraw.Draw(base)
-   name.text((0, 0), person['First'], fill="Black")
+   name.fontmode = "1" # this apparently sets (anti)aliasing
+   name.text((0, 0), person['First'], font=fontBig, fill="Black")
 
    # composite the final image
    #out = Image.alpha_composite(base, name)
